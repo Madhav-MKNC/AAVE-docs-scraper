c2d8c35ffd036459173bb734a37913e69202f1d1
MKNC <madhavampire@gmail.com>
2023-01-12 03:20:57 +0530

    fully working now

--- aave scraper v2.py
+++ aave scraper v2.py
@@ -13,41 +13,63 @@ from urllib.parse import urljoin
 
 import os
 from platform import uname
-def clearScreen():
+def printScreen():
     os.system('cls' if 'win' in uname().system.lower() else 'clear')
+    print("<"+"="*50+" AAVE's Docs Scraper "+"="*50+">\n")
 
-from time import time as now 
+from time import time 
 def makeDir(dname):
-    now = str(int(now()))
+    now = str(int(time()))
     if os.path.exists(dname): 
-        print(f"[-] '{dname}' directory already Exists, creating and saving all the data in '{dname} {now}/' directory")
+        print(f"[-] '{dname}/' directory already Exists, creating and saving all the data in '{dname} {now}/' directory")
         dname = f"{dname} {now}"
     else:
         print(f"[+] Creating directory '{dname}'")
-        os.mkdir(dname)
+    os.mkdir(dname)
 
 # for saving the data
 def saveData(fname, data):
     with open(fname,'w',encoding="utf-8") as file:
         file.write(str(data))
 
-# scraping links
-def get_paths(domain, url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+# links crawler class
+class Crawler:
+    def __init__(self, base_url, restricted_domain='https://'):
+        self.urls = [base_url]
+        self.links_found = [base_url]
+        self.restricted_domain = restricted_domain
 
-    urls = [url]
-    for link in soup.find_all('a'):
-        path = link.get('href')
-        if path.startswith('/'):
-            path = urljoin(url,path)
-        if path.startswith(domain) and '#' not in path: 
-            urls.append(path)
+    def fetch_all_links(self, url):
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        for link in soup.find_all('a'):
+            path = link.get('href')
+            if not path.startswith('http'): path = urljoin(url,path)
 
-    return list(set(urls))
+            if '#' in path: continue
+            if not path.startswith(self.restricted_domain): continue
+            if path in self.links_found: continue
+
+            print("[+] Link found -",path)
+            self.urls.append(path)
+            self.links_found.append(path)
+
+    def crawl(self, crawl_limit=20):
+        while self.urls:
+            if len(self.links_found)>crawl_limit: break
+
+            url = self.urls.pop(0)
+            print("[+] Crawling:",url)
+            try:
+                self.fetch_all_links(url)
+            except AttributeError:
+                print(f'[-] No href on : {url}\n')
+            except Exception as e:
+                print(f'[!] Failed to crawl: {url}')
+                print("[!] REASON:",e,'\n')
 
 # page content
-def fetch_data(url, content_class, dirName='data'):
+def fetch_data(url, content_class, dirName='.'):
     try:
         print("[+] Scraping",url)
         response = requests.get(url)
@@ -68,41 +90,34 @@ def fetch_data(url, content_class, dirName='data'):
     saveData(fileName, content)
 
 
-def crawl(url,content_class,root_dir):
-    makeDir(root_dir)
-    
-    fetch_data(url,url,content_class,dirName=root_dir)
-    print(f"[+] Scraping all the links found on - {url}\n")
+def scrape_all(url,content_class,dirName):
+    domain = '/'.join(url.split('/')[0:3])+'/'
+    aave = Crawler(base_url=url, restricted_domain=domain)
+    aave.crawl(crawl_limit=20)
+    saveData(f'{dirName}/links_found.txt',"/n".join(aave.links_found))
 
-    try:
-        print(get_paths(url))
-        print(url)
-        for path in get_paths(url):
-            print(1)
-            try:
-                for link in get_paths(path):
-                    print("[+] Scraping",link)
-                    fetch_data(url,link, data_class,dirName='data')
-            except Exception as e:
-                print("[!] Failed to fetch",link)
-                print("[=]",e)
-    except KeyboardInterrupt:
-        print("[-] KeyBoard interrupted")
-        print("[-] Exiting...")
+    for path in aave.links_found:
+        try:
+            print("[+] Scraping",path)
+            fetch_data(path, content_class,dirName)
+        except Exception as e:
+            print("[!] Failed to fetch",path)
+            print("[ERROR]",e)
+
 
     
 if __name__ == "__main__":
-    clearScreen()
-    print("AAVE's Docs Scraper\n")
-
+    printScreen()
+    
     root_dir = input("[=] Enter a root directory name: ")
     print(f"[+] Data will saved in '{root_dir}' in the currect directory")
+    makeDir(root_dir)
 
-    
-    url = "https://docs.aave.com/hub/"
-    if 'http' not in url: 
-        print('[!] Enter full url, for eg. https://example.xyz/')
-        exit()
-
+    url = "https://docs.aave.com/"
     data_class = "css-175oi2r r-bnwqim"
-    crawl(url,data_class,root_dir)
+
+    try:
+        scrape_all(url,data_class,root_dir)
+    except KeyboardInterrupt:
+        print("[-] KeyBoard interrupted")
+        print("Exiting...")
