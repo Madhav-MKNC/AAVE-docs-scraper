cbd7d7aac22488447c21e18cce5b421c765dbf23
Madhav Kumar <78339236+Madhav-MKNC@users.noreply.github.com>
2023-03-05 16:27:39 +0530

    maxxxx

--- aave scraper v2.py
+++ aave scraper v2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 # Title:    AAVE's Docs Scraper
 # Author:   MKNC
 # created:  10-01-2023 19:30 IST
@@ -9,7 +10,7 @@
 # BeautifulSoup --> for parsing the html data in a tree like structure
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
 
 import os
 from platform import uname
@@ -34,47 +35,48 @@ def saveData(fname, data):
 
 # links crawler class
 class Crawler:
-    def __init__(self, base_url, restricted_domain='https://'):
+    def __init__(self, base_url, restricted_domain='', crawl_limit=20):
         self.urls = [base_url]
-        self.links_found = [base_url]
+        self.links_found = []
         self.restricted_domain = restricted_domain
+        self.crawl_limit = crawl_limit
+        self.index = 1
 
     def fetch_all_links(self, url):
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
         for link in soup.find_all('a'):
+            if len(self.links_found)>self.crawl_limit: return
+
             path = link.get('href')
             if not path.startswith('http'): path = urljoin(url,path)
 
             if '#' in path: continue
-            if not path.startswith(self.restricted_domain): continue
+            if urlparse(path).hostname != self.restricted_domain and self.restricted_domain!='': continue
             if path in self.links_found: continue
 
-            print("[+] Link found -",path)
+            print("[*] Link found -",path)
             self.urls.append(path)
             self.links_found.append(path)
 
-    def crawl(self, crawl_limit=20):
+    def crawl(self):
         while self.urls:
-            if len(self.links_found)>crawl_limit: break
-
             url = self.urls.pop(0)
-            print("[+] Crawling:",url)
+            print("\n[+] Crawling:",url)
             try:
                 self.fetch_all_links(url)
-            except AttributeError:
-                print(f'[-] No href on : {url}\n')
             except Exception as e:
                 print(f'[!] Failed to crawl: {url}')
                 print("[!] REASON:",e,'\n')
 
 # page content
-def fetch_data(url, content_class, dirName='.'):
+def fetch_data(self, url, content_class, dirName='.'):
     try:
-        print("[+] Scraping",url)
+        print(f"[{self.index}] Scraping",url)
+        self.index += 1
         response = requests.get(url)
         data = BeautifulSoup(response.text, 'html.parser')
-        title = data.find('title').text
+        # title = data.find('title').text     # for filename = title of page
         data = data.find_all(class_=content_class)[1:]      # skips the first element
     except Exception as e:
         print("[!] Failed to fetch",url)
@@ -82,18 +84,19 @@ def fetch_data(url, content_class, dirName='.'):
         return
 
     # saving data
+    title = urlparse(url).path[1:-1].replace('/','-')
     fileName = f"{dirName}/{title}.txt"
     content = f"[{title}]\n"
     content += f"[{url}]]\n"
     for i in data:
-        content+='\n'+i.text.strip()+'\n'
+        content += '\n'+i.text.strip()+'\n'
     saveData(fileName, content)
 
 
-def scrape_all(url,content_class,dirName):
-    domain = '/'.join(url.split('/')[0:3])+'/'
-    aave = Crawler(base_url=url, restricted_domain=domain)
-    aave.crawl(crawl_limit=20)
+def scrape_all(url, content_class, dirName, crawl_limit):
+    domain = urlparse(url).hostname
+    aave = Crawler(base_url=url, restricted_domain=domain, crawl_limit=crawl_limit)
+    aave.crawl()
     saveData(f'{dirName}/links_found.txt',"/n".join(aave.links_found))
 
     for path in aave.links_found:
@@ -114,10 +117,17 @@ if __name__ == "__main__":
     makeDir(root_dir)
 
     url = "https://docs.aave.com/"
+    ## UPDATE THIS VALUE IF NO DATA IS FETCHED
+    # it might be possible that the website developers may have changed the content class or maybe just modified
     data_class = "css-175oi2r r-bnwqim"
 
+    crawl_limit = input("[=] Enter a crawl limit: ")
+    if not crawl_limit.isnumeric():
+        print("[!] Enter a valid number")
+        exit()
+
     try:
-        scrape_all(url,data_class,root_dir)
+        scrape_all(url, data_class, root_dir, int(crawl_limit))
     except KeyboardInterrupt:
         print("[-] KeyBoard interrupted")
         print("Exiting...")
